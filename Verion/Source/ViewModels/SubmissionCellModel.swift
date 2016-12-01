404ffd51627f2c9e3bf7ec50c2ca05486718699e
Simon Chen <fizzyfrosty@yahoo.com>
2016-12-01 15:25:45 -0500

    Started on view model

---
+++ Verion/Source/ViewModels/SubmissionCellModel.swift
@@ -0,0 +1,13 @@
+//
+//  SubmissionCellModel.swift
+//  Verion
+//
+//  Created by Simon Chen on 12/1/16.
+//  Copyright © 2016 Workhorse Bytes. All rights reserved.
+//
+
+import UIKit
+
+class SubmissionCellModel{
+    
+}
