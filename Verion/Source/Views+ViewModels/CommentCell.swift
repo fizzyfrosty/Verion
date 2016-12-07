402ae2f35622ffafa4a31fbaaf0ca4780e1f0573
Simon Chen <fizzyfrosty@yahoo.com>
2016-12-06 22:59:31 -0500

    Started adding Comments section

---
+++ Verion/Source/Views+ViewModels/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Verion
+//
+//  Created by Simon Chen on 12/6/16.
+//  Copyright © 2016 Workhorse Bytes. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Verion/Source/Views+ViewModels/CommentCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CommentCellViewModel.swift
+//  Verion
+//
+//  Created by Simon Chen on 12/6/16.
+//  Copyright © 2016 Workhorse Bytes. All rights reserved.
+//
+
+import UIKit
+
+class CommentCellViewModel {
+
+}
